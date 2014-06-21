3b97106f8d9b43968235d76893731b8f1ba1bdfc
Daniel Abraham <abrahamd@akamai.com>
2014-06-20 21:01:21 -0400

    altered to return actual HTML from an object, not not-HTML from a string...

--- src/main/java/org/sweatshop/hello/restApi/HelloWorld.java
+++ src/main/java/org/sweatshop/hello/restApi/HelloWorld.java
@@ -4,13 +4,29 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+import lombok.Value;
 
 @Path("/world")
 public class HelloWorld {
 
+    @Value
+    private class HtmlTextObject {
+        String text;
+        
+        public String toHtml() {
+            return new StringBuilder()
+            .append("<HTML><BODY>")
+            .append(text)
+            .append("</BODY></HTML>")
+            .toString();
+        }
+    }
+    
     @GET
     @Produces(MediaType.TEXT_HTML)
-    public String sayHtmlHello() {
-        return "Hello world! ";
+    public Response sayHtmlHello() {
+        return Response.ok(new HtmlTextObject("Hello world!").toHtml()).build();
     }
 }
